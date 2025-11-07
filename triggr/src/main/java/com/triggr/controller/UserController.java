5d59a2f51a1cb38171387da569cfb90cc59195a1
Vaibhav <22838.vaibhav@gmail.com>
2025-11-07 01:36:32 -0500

    User logic init

---
+++ triggr/src/main/java/com/triggr/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.triggr.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.triggr.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
