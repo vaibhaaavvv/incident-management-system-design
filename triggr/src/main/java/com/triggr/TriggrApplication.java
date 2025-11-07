5cdd9a503ec3d4540795f51083741ca9223d9ec8
Vaibhav <22838.vaibhav@gmail.com>
2025-11-07 01:24:50 -0500

    spring init

---
+++ triggr/src/main/java/com/triggr/TriggrApplication.java
@@ -0,0 +1,13 @@
+package com.triggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriggrApplication.class, args);
+	}
+
+}
---
+++ triggr/src/test/java/com/triggr/TriggrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triggr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriggrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
